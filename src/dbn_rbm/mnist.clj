9b4eb5db79db7415a6f6469f7e392b258dc716d5
Chris Sims <chris@jcsi.ms>
2014-09-02 15:36:37 -0700

    Initial RBM testing.

--- src/dbn_rbm/mnist.clj
+++ src/dbn_rbm/mnist.clj
@@ -1,10 +1,9 @@
 (ns dbn-rbm.mnist
-  (:require [clojure.data.csv :as csv]
+  (:require [dbn-rbm.rbm :as rbm]
+            [clojure.data.csv :as csv]
             [clojure.java.io :as io]
             [clojure.core.matrix :as matrix]
             [clojure.core.matrix.operators]))
-
-(matrix/set-current-implementation :vectorz)
 
 (defn load-csv
   "Load the CSV into memory"
@@ -15,18 +14,26 @@
 (defn scale-data
   "Scale the input parameters to [0-1]"
   [x]
-  (conj (doall (mapv #(/ % 255.0) (rest x))) (first x)))
+  (conj (mapv #(/ % 255.0) (rest x)) (first x)))
 
 (defn load-data
   "Load a MNIST CSV data set."
   [filepath]
-  (let [input (load-csv filepath)
-        m (matrix/matrix input)
-        read-data (matrix/emap read-string m)]
-    (doall (mapv scale-data read-data))))
+  (->> filepath
+       (load-csv)
+       (matrix/matrix)
+       (matrix/emap read-string)
+       (mapv scale-data)))
 
 (defn load-data-sans-label
   "Load a MNIST CSV data set without the label"
   [filepath]
   (let [data (load-data filepath)]
     (matrix/select data :all (range 0 784))))
+
+(defn load-data-with-softmax
+  "Load a dataset with the class label expanded to a softmax-appropriate form.
+  Example: In the MNIST dataset, class '7' expands to -> '0 0 0 0 0 0 0 1 0 0"
+  [filepath]
+  (let [data (load-data filepath)]
+    (doall (mapv #(rbm/softmax-from-obv % 10) data))))
--- src/dbn_rbm/rbm.clj
+++ src/dbn_rbm/rbm.clj
@@ -17,7 +17,6 @@
 (defn build-rbm
   "Factory function to produce an RBM record."
   [visible hidden]
-  (set-current-implementation :vectorz)
   (let [w (matrix (take visible
                         (repeatedly #(rand-vec hidden 0.01))))
         hbias (zero-vector hidden)
@@ -27,56 +26,156 @@
         vbias (zero-vector visible)]
     (->RBM w hbias vbias visible hidden)))
 
+(defn build-jd-rbm
+  "Build a joint density RBM for testing purposes.
+
+  This RBM has two sets of visible units - the typical set
+  representing each observation in the data set, and a softmax unit
+  representing the label for each observation."
+  [visible hidden classes]
+  (build-rbm (+ visible classes) hidden))
+
 (defn update-weights
   "Determine the weight gradient from this batch"
-  [ph ph2 batch v]
+  [ph ph2 batch pv]
   (let [updated (pmap #(- (outer-product % %2) (outer-product %3 %4))
-                     (rows batch) (rows ph)
-                     (rows v)     (rows ph2))]
+                      (rows batch) (rows ph)
+                      (rows pv)    (rows ph2))]
     (reduce + updated)))
 
 (defn update-rbm
   "Single batch step update of RBM parameters"
   [batch rbm learning-rate]
-  (let [ph (emap sigmoid (+ (:hbias rbm) (mmul batch (:w rbm))))
+  (let [batch-size (row-count batch)
+        ph (emap sigmoid (+ (:hbias rbm) (mmul batch (:w rbm))))
         h (emap bernoulli ph)
         pv (emap sigmoid (+ (:vbias rbm)
                             (mmul h (transpose (:w rbm)))))
         v (emap bernoulli pv)
         ph2 (emap sigmoid (+ (:hbias rbm) (mmul v (:w rbm))))
-        delta-w (update-weights ph ph2 batch v)
-        delta-vbias (reduce + (map #(- % %2)
-                                   (rows batch)
-                                   (rows v)))
-        delta-hbias (reduce + (map #(- % %2)
-                                   (rows h)
-                                   (rows ph2)))]
+        delta-w (/ (update-weights ph ph2 batch pv) batch-size)
+        delta-vbias (/ (reduce + (map #(- % %2)
+                                      (rows batch)
+                                      (rows pv)))
+                       batch-size)
+        delta-hbias (/ (reduce + (map #(- % %2)
+                                      (rows h)
+                                      (rows ph2)))
+                       batch-size)]
     (assoc rbm
       :w (+ (:w rbm) (mmul learning-rate delta-w))
       :vbias (+ (:vbias rbm) delta-vbias)
       :hbias (+ (:hbias rbm) delta-hbias))))
 
-(defn train-rbm
-  "Given a training set, train an RBM"
-  [rbm train-set learning-rate batch-size epochs]
+(defn train-epoch
+  "Train a single epoch"
+  [rbm train-set learning-rate batch-size]
   (let [columns (column-count train-set)]
     (loop [rbm rbm
            batch (array (submatrix train-set 0 batch-size 0 columns))
            batch-num 1]
       (let [start (* (dec batch-num) batch-size)
             end (min (* batch-num batch-size) (row-count train-set))]
-        (if (== end (row-count train-set))
+        (if (>= start (row-count train-set))
           rbm
-          (recur (update-rbm batch rbm learning-rate)
-                 (array (submatrix train-set start (- end start) 0 columns))
-                 (inc batch-num)))))))
+          (do
+            (println "Batch:" batch-num)
+            (recur (update-rbm batch rbm learning-rate)
+                   (array (submatrix train-set start (- end start) 0 columns))
+                   (inc batch-num))))))))
 
-(defmethod clojure.core/print-method RBM [rbm ^Writer w]
-  (.write w (str "RBM with " (:visible rbm) " visible units "))
-  (.write w (str "and " (:hidden rbm) " hidden units.\n")))
+(defn train-rbm
+  "Given a training set, train an RBM"
+  [rbm train-set learning-rate batch-size epochs]
+  (loop [rbm rbm
+         epoch 1]
+    (if (> epoch epochs)
+      rbm
+      (do
+        (println "Training epoch" epoch)
+        (recur (train-epoch rbm train-set learning-rate batch-size)
+               (inc epoch))))))
 
 (defn sigmoid
   "Sigmoid function, used as an activation function for nodes in a
   network."
   [^double x]
   (/ (+ 1 (Math/exp (* -1 x)))))
+
+(defn free-energy
+  "Compute the free energy of a given visible vector and RBM. Lower is
+  better."
+  [x rbm]
+  (let [hidden-input (+ (:hbias rbm) (mmul x (:w rbm)))]
+    (- (- (mmul x (:vbias rbm)))
+       (reduce + (mapv #(Math/log (+ 1 (Math/exp %))) hidden-input)))))
+
+
+;;==============================================================================
+;; Testing an RBM trained on a data set
+;;==============================================================================
+
+
+(defn gen-softmax
+  "Generate a softmax output. x is the class represented by the
+  output, with 0 represented by the first element in the vector."
+  [x num-classes]
+  (mset (zero-vector num-classes) x 1))
+
+(defn softmax-from-obv
+  "Given an observation with label attached, replace the label value
+  with an appropriate softmax unit. This assumes that the label is the
+  last element in an observation."
+  [x num-classes]
+  (let [label (peek x)
+        obv (pop x)
+        new-label (gen-softmax label num-classes)]
+    (vec (concat new-label obv))))
+
+(defn get-min-position
+  "Get the position of the minimum element of a collection.
+
+  TODO: This may not be the best approach."
+  [x]
+  (if (not (empty? x))
+    (let [least (reduce min x)
+          indexed (zipmap x (range (count x)))]
+      (get indexed least))))
+
+(defn get-prediction
+  "For a given observation and RBM, return the predicted class."
+  [x rbm num-classes]
+  (let [softmax-cases (mapv #(gen-softmax % num-classes) (range num-classes))
+        trials (mapv #(vec (concat % %2)) softmax-cases (repeat (pop x)))
+        results (mapv #(free-energy % rbm) trials)]
+    (get-min-position results)))
+
+(defn test-rbm
+  "Test a joint density RBM trained on a data set. Returns an error
+  percentage."
+  [rbm test-set num-classes]
+  (let [num-observations (row-count test-set)
+        predictions (doall (pmap #(get-prediction % rbm num-classes) test-set))
+        errors (doall (map #(if (== (peek %) %2) 0 1) test-set predictions))
+        total (reduce + errors)]
+    (double (/ total num-observations))))
+
+;;==============================================================================
+;; Utility functions for an RBM
+;;==============================================================================
+
+;; TODO: This could be drastically improved to provide better
+;; information about the RBM.
+(defmethod clojure.core/print-method RBM [rbm ^Writer w]
+  (.write w (str "RBM with " (:visible rbm) " visible units "))
+  (.write w (str "and " (:hidden rbm) " hidden units.\n")))
+
+(defn save-rbm
+  "Save a RBM to disk."
+  [rbm filepath]
+  )
+
+(defn load-rbm
+  "Load a RBM from disk."
+  [filepath]
+  )
