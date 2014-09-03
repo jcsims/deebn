c69c87e33b22a0fb477acd2848faef1c88ea6944
Chris Sims <chris@jcsi.ms>
2014-09-03 11:47:50 -0700

    Implement mean for model inspection.

--- src/dbn_rbm/util.clj
+++ src/dbn_rbm/util.clj
@@ -1,6 +1,12 @@
-(ns dbn-rbm.util)
+(ns dbn-rbm.util
+  (:use [clojure.core.matrix]))
 
 (defn bernoulli
   "Take a single Bernoulli sample, given a probability"
   [p]
   (if (> (rand) p) 0 1))
+
+(defn mean
+  "Find the mean (average) value of a vector."
+  [^doubles x]
+  (double (/ (reduce + x) (row-count x))))
