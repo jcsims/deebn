7589a40668198c8dcb91774d489ae4ae498671d9
Chris Sims <chris@jcsi.ms>
2014-11-20 13:28:31 -0800

    Prepare for a 0.1.1 release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject deebn "1.0.0-SNAPSHOT"
+(defproject deebn "0.1.1-SNAPSHOT"
   :description "Deep Belief Network using Restricted Boltzmann Machines"
   :url "https://jcsi.ms"
   :license {:name "Eclipse Public License"
